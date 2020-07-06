f180fdd894403e841fbb96cda16e8286879b612b
Tony380 <tonygon2@yahoo.es>
2020-07-06 11:53:43 -0400

    comments parser test

--- app/tests/test_parse.py
+++ app/tests/test_parse.py
@@ -1,11 +1,11 @@
 """This file contains the parser test"""
 from app.program.parser import Parser
 
-question = Parser("Salut Grandpy, ca va?? Est-ce que tu sais où se trouve le Louvre?????")
+question = Parser("Salut Grandpy!!, ca va?? Est-ce que tu sais où se trouve le Louvre???")
 
 
 class TestParser:
-
+    """Test class"""
     def test_parse(self):
         result = question.parse()
         assert result == "louvre"
