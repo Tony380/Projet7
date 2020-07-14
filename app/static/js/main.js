e247cde92eb43a7464187e80fe75f2c198b6083b
Tony380 <tonygon2@yahoo.es>
2020-07-14 10:58:35 -0400

    update program

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,24 +2,8 @@ var input = document.getElementById("input");
 var map = document.getElementById("container");
 var button = document.getElementById("button");
 
+
 button.addEventListener("click", function(){
-    var div = document.createElement("div");
-    var div1 = document.createElement("div");
-    var div2 = document.createElement("div");
-    var div3 = document.createElement("div");
-    div.setAttribute("id", "input_value");
-    div1.setAttribute("id", "adress");
-    div2.setAttribute("id", "wiki");
-    div3.setAttribute("id", "map");
-    map.appendChild(div);
-    map.appendChild(div1);
-    map.appendChild(div2);
-    map.appendChild(div3);
-    function initMap(lat, lng){
-        var place = {lat: lat, lng: lng};
-        var map = new google.maps.Map(div3, {zoom: 10, center: place});
-        var marker = new google.maps.Marker({position: place, map: map});
-}
 
     var request = new XMLHttpRequest();
     request.open("get", "/api?question=" + input.value);
@@ -28,7 +12,19 @@ button.addEventListener("click", function(){
 
     request.onload = function(){
         var answer = this.response;
-        div.innerHTML = input.value
+        var div = document.createElement("div");
+        var div1 = document.createElement("div");
+        var div2 = document.createElement("div");
+        var div3 = document.createElement("div");
+        div.setAttribute("id", "input_value");
+        div1.setAttribute("id", "adress");
+        div2.setAttribute("id", "wiki");
+        div3.setAttribute("id", "map");
+        map.appendChild(div);
+        map.appendChild(div1);
+        map.appendChild(div2);
+        map.appendChild(div3);
+        div.innerHTML = input.value;
         div1.innerHTML = "Bien sûr mon poussin, voici l'adresse : " + answer['coords']['adress'];
 
         var a = document.createElement('a');
@@ -36,6 +32,12 @@ button.addEventListener("click", function(){
         a.appendChild(link);
         a.title = " En savoir plus sur Wikipedia";
         a.href = answer['url'];
+
+        function initMap(lat, lng){
+        var place = {lat: lat, lng: lng};
+        var map = new google.maps.Map(div3, {zoom: 10, center: place});
+        var marker = new google.maps.Marker({position: place, map: map});
+        }
 
         div2.innerHTML = "Au fait, je ne t'ai pas raconté. " + answer['page'];
         div2.append(a);
