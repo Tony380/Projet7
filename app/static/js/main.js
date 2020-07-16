61f48bceb4174cd56cf014edf66beb3de6896179
Tony380 <tonygon2@yahoo.es>
2020-07-16 11:46:17 -0400

    Corrections main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -10,41 +10,49 @@ function display(){
 
     request.onload = function(){
         var answer = this.response;
+        if (Object.keys(answer).length > 1){
+            var div = document.createElement("div");
+            var div1 = document.createElement("div");
+            var div2 = document.createElement("div");
+            var div3 = document.createElement("div");
 
-        var div = document.createElement("div");
-        var div1 = document.createElement("div");
-        var div2 = document.createElement("div");
-        var div3 = document.createElement("div");
+            div.setAttribute("id", "input_value");
+            div1.setAttribute("id", "adress");
+            div2.setAttribute("id", "wiki");
+            div3.setAttribute("id", "map");
 
-        div.setAttribute("id", "input_value");
-        div1.setAttribute("id", "adress");
-        div2.setAttribute("id", "wiki");
-        div3.setAttribute("id", "map");
+            container.appendChild(div);
+            container.appendChild(div1);
+            container.appendChild(div2);
+            container.appendChild(div3);
 
-        container.appendChild(div);
-        container.appendChild(div1);
-        container.appendChild(div2);
-        container.appendChild(div3);
+            div.innerHTML = input.value;
+            div1.innerHTML = "Bien sûr mon poussin, voici l'adresse : " + answer['adress'];
 
-        div.innerHTML = input.value;
-        div1.innerHTML = "Bien sûr mon poussin, voici l'adresse : " + answer['adress'];
+            var a = document.createElement('a');
+            var link = document.createTextNode(" En savoir plus sur Wikipedia");
+            a.appendChild(link);
+            a.title = " En savoir plus sur Wikipedia";
+            a.href = answer['url'];
 
-        var a = document.createElement('a');
-        var link = document.createTextNode(" En savoir plus sur Wikipedia");
-        a.appendChild(link);
-        a.title = " En savoir plus sur Wikipedia";
-        a.href = answer['url'];
+            function initMap(lat, lng){
+            var place = {lat: lat, lng: lng};
+            var map = new google.maps.Map(div3, {zoom: 10, center: place});
+            var marker = new google.maps.Marker({position: place, map: map});
+            }
 
-        function initMap(lat, lng){
-        var place = {lat: lat, lng: lng};
-        var map = new google.maps.Map(div3, {zoom: 10, center: place});
-        var marker = new google.maps.Marker({position: place, map: map});
+            div2.innerHTML = "Au fait, je ne t'ai pas raconté. " + answer['page'];
+            div2.append(a);
+            initMap(answer['lat'], answer['lng']);
+            input.value = '';
         }
-
-        div2.innerHTML = "Au fait, je ne t'ai pas raconté. " + answer['page'];
-        div2.append(a);
-        initMap(answer['lat'], answer['lng']);
-        input.value = '';
+        else{
+            var div = document.createElement("div");
+            div.setAttribute("id", "adress");
+            container.appendChild(div);
+            div.innerHTML = "Je ne me souviens plus très bien. Tu peux préciser?";
+            input.value = '';
+        }
     }
 }
 
