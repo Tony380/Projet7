7c0ce6a39acb19d6aa1ad70f07e6c64062d1d8bb
Tony380 <tonygon2@yahoo.es>
2020-07-18 13:41:53 -0400

    comments parser

--- app/program/parser.py
+++ app/program/parser.py
@@ -14,13 +14,17 @@ class Parser:
         """ Parsing function """
         # Sets words in lowercase
         self.question = self.question.lower()
+
         # Removes accents
         self.question = ''.join((word for word in unicodedata.normalize('NFD', self.question)
                                  if unicodedata.category(word) != 'Mn'))
+
         # Removes punctuation and converts the question in a list of words
         self.question = re.sub(r'[^\w]', ' ', self.question).split()
+
         # Isolates keywords
         self.question = [word for word in self.question if word not in STOP_WORDS]
+
         # Converts the list into a string
         self.question = ' '.join(self.question)
         return self.question
