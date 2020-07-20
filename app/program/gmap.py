01ead4aed41380bef655c36a9ed1f60c58f13545
Tony380 <tonygon2@yahoo.es>
2020-07-19 21:15:51 -0400

    Correction gmap

--- app/program/gmap.py
+++ app/program/gmap.py
@@ -7,15 +7,17 @@ class Gmap:
     """Uses google's API to fetch coordinates and adress from a place"""
     def __init__(self, question):
         self.question = question
-        self.endpoint = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+        self.url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
         self.location = '&components=country:FR&key='
         self.key = API_KEY
 
     def search(self):
         """Search coordinates and adress"""
         try:
-            request = requests.get(self.endpoint + self.question + self.location + self.key)
-            result = request.json()
+            req = requests.get(self.url + self.question +
+                               self.location + self.key)
+
+            result = req.json()
             adress = result['results'][0]['formatted_address']
             lat = result['results'][0]['geometry']['location']['lat']
             lng = result['results'][0]['geometry']['location']['lng']
